1b5071964becbf4cf74052263ccda5059ae42c09
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-29 10:10:33 +0530

    userinfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PassengerDataComponent } from './passenger-data/passenger-data.component';
 import { UsDataComponent } from './us-data/us-data.component';
 import { PublicApiComponent } from './public-api/public-api.component';
+import { UserInfoiComponent } from './user-infoi/user-infoi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PassengerDataComponent,
     UsDataComponent,
-    PublicApiComponent
+    PublicApiComponent,
+    UserInfoiComponent
   ],
   imports: [
     BrowserModule,
